11a74cc9e683fed50547c15e87bf4901c3bb9dd4
Samuel Soyebo <ayodejisoyebo@gmail.com>
2023-01-28 22:48:23 +0100

    Hamburger Integrated

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import styles from "./Homepage.module.scss"
+import Button from "../../components/button/button";
 const Homepage: FC = ()=> {
   return (
-      <div className={styles.Container}>
-          <Navbar/>
+    <div className={styles.Container}>
+      <Navbar />
+      <Button />
       <div className={styles.Hero}>
-        <h1>Your Reliable Doortep</h1>
+        <h1>Your Reliable Doorstep Delivery.</h1>
+        <p>Quick, Reliable And Affordable With Realtime Tracking</p>
+
+        <div className={styles.Hero_btn}></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Homepage
